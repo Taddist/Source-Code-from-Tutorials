693cac6da312bae91d29d67ebf51d4f505729fcb
Taddist <afaf.taddist71@gmail.com>
2017-10-09 13:07:24 +0100

    Configure DB

--- drf_tutorial/drf_tutorial/settings.py
+++ drf_tutorial/drf_tutorial/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'drf_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangorestframework',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
